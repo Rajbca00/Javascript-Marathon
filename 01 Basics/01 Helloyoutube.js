f813e1b7a0b10ec20629c746aa9807e0425ac3a9
Rajesh S <rajbca00@gmail.com>
2022-06-21 22:22:11 +0530

    Day1Completed

---
+++ 01 Basics/01 Helloyoutube.js
@@ -0,0 +1,67 @@
+console.log("Hello youtube stream")
+
+//Variable declaration
+
+let username = "Rajesh Sambasivam"
+username='youtube'
+
+let userMobile = 9898989898
+let isLoggedIn = true
+
+let testMeLet
+var testMeVar // Not a recommended way to declare variables
+
+console.log(testMeLet) //nothing
+console.log(testMeVar) //nothing
+
+//Lexical Structure - Elementary Rules for Language specification
+// Javascript is case-sensitive
+
+// 1. comments 
+// Shortcut - ctrl+/
+// /* Multi Line comments */
+
+
+// 2. Literals
+
+/*
+2323
+23.5
+'string type one'
+"string type two"
+true
+false
+null
+*/
+
+ 
+// 3. reserverd Keywords
+
+// enum
+// implements
+// interface
+// package
+// private
+// protected
+// public
+// More are there, These are future reserved keywords
+
+
+// 4. Unicode
+
+console.log("\u00E9")
+console.log("\u0301")
+
+// const cafē = 1
+// const cafē = 2
+
+// console.log(cafē)
+
+// 5. semi colons
+
+let gameScore 
+= 289
+console.log(gameScore);
+
+let y = x + f
+(a+b).toString()
